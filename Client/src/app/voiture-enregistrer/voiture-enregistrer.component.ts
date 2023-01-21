b6828886e93e79efb2a57536fd8f5896a8859f9e
RindratianaRatsimandavana <rindratianaratsimandavana@gmail.com>
2023-01-21 13:07:42 +0300

    commit client interface src

---
+++ Client/src/app/voiture-enregistrer/voiture-enregistrer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voiture-enregistrer',
+  templateUrl: './voiture-enregistrer.component.html',
+  styleUrls: ['./voiture-enregistrer.component.scss']
+})
+export class VoitureEnregistrerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
