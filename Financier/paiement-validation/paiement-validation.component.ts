7bccd98087e19f19154063cd5a0a876596312c5d
onytianarazafy <razafyonytiana@gmail.com>
2023-01-22 16:26:49 +0300

    respo financier

---
+++ Financier/paiement-validation/paiement-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paiement-validation',
+  templateUrl: './paiement-validation.component.html',
+  styleUrls: ['./paiement-validation.component.scss']
+})
+export class PaiementValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
