a896c504faa189ca320a9f3e9c582bf7c19a7012
RindratianaRatsimandavana <rindratianaratsimandavana@gmail.com>
2023-01-31 11:12:45 +0300

    deployUpdate

--- Financier_O/server.js
+++ Financier_O/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Financier_O'));
+app.use(express.static(__dirname + '/dist/financier-o'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/Financier_O/index.html'));
+res.sendFile(path.join(__dirname+'/dist/financier-o/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
--- FrontM/server.js
+++ FrontM/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Front_M'));
+app.use(express.static(__dirname + '/dist/front-moi'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/Front_M/index.html'));
+res.sendFile(path.join(__dirname+'/dist/front-moi/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
